a2e06d0b1b773705ad93a7366ecefa1c7c6c883c
Aminul Ali <aminulali000@gmail.com>
2020-05-25 19:28:33 +0100

    Updates

---
+++ precache-manifest.0f1c11e1e5fb245cc53e4a7c293bcbdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88175a0c0a90dc4b166d11c5bb886159",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "9ce89c64d9084d7b0081",
+    "url": "/movie-search/static/css/main.1348ed14.chunk.css"
+  },
+  {
+    "revision": "3a95e1c60cf84eacb68d",
+    "url": "/movie-search/static/js/2.4e3c479e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.4e3c479e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce89c64d9084d7b0081",
+    "url": "/movie-search/static/js/main.f38f7c58.chunk.js"
+  },
+  {
+    "revision": "af50e72dfcbbabd90d7f",
+    "url": "/movie-search/static/js/runtime-main.4ec281b5.js"
+  }
+]);
